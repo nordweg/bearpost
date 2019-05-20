54b30e1ae097f088cfcf8f67d10ed20f3b132ca1
Lucas Kuhn <lucasalexkuhn@gmail.com>
2019-05-20 16:05:06 -0300

    Improves shipments layout

--- app/models/shipment.rb
+++ app/models/shipment.rb
@@ -2,10 +2,14 @@ class Shipment < ApplicationRecord
   # validates_uniqueness_of :shipment_number, scope: :order_number
   # validates_presence_of   :invoice_series
   # validates_presence_of   :invoice_number
-  # If we want unique invoice numbers in a series 
+  # If we want unique invoice numbers in a series
   # validates_uniqueness_of :invoice_number, scope: :invoice_series
 
   def shipped?
     shipped_at.present?
+  end
+
+  def full_name
+    "#{recipient_first_name} #{recipient_last_name}"
   end
 end
