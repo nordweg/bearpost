b9c2c376a788a5d3138b969dc489f3b5cffc246d
Lucas Kuhn <lucasalexkuhn@gmail.com>
2019-05-21 10:17:54 -0300

    cleans unused specs, improves layout for shipments CRUD actions

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190514184503) do
+ActiveRecord::Schema.define(version: 20190520203926) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -71,6 +71,8 @@ ActiveRecord::Schema.define(version: 20190514184503) do
     t.string "recipient_city"
     t.string "recipient_city_code"
     t.string "recipient_state"
+    t.string "sender_country"
+    t.string "recipient_country"
   end
 
 end
--- spec/views/shipments/show.html.erb_spec.rb
+++ spec/views/shipments/show.html.erb_spec.rb
@@ -3,12 +3,12 @@ require 'rails_helper'
 RSpec.describe "shipments/show", type: :view do
   before(:each) do
     @shipment = assign(:shipment, Shipment.create!(
-      :shipment_number => "Shipment Number"
+      :shipment_number => "R123"
     ))
   end
 
-  it "renders attributes in <p>" do
+  it "Renders newly created shipment attributes" do
     render
-    expect(rendered).to match(/Shipment Number/)
+    expect(rendered).to match(/R123/)
   end
 end
