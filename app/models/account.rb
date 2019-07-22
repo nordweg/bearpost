aeba50f456ad897b076210b2b8fe4f21f94e7958
Lucas Kuhn <lucasalexkuhn@gmail.com>
2019-07-22 15:13:40 -0300

    adds belongs to on account

--- app/models/account.rb
+++ app/models/account.rb
@@ -2,6 +2,7 @@ class Account < ApplicationRecord
   default_scope { order(name: :asc) }
 
   has_many :shipments
+  belongs_to  :company
 
   def selected_shipping_methods(carrier)
     # selected_shipping_methods = []
