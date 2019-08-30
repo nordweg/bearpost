7aee9ccce98087af1ea375078b31f3d4e2f65bea
Lucas Kuhn <lucasalexkuhn@gmail.com>
2019-08-30 14:43:19 -0300

    updates method to get delivery updates and history show

--- app/models/history.rb
+++ app/models/history.rb
@@ -1,6 +1,7 @@
 class History < ApplicationRecord
-  default_scope { order(created_at: :asc) }
   belongs_to :shipment
   # belongs_to :user, optional: true, default: -> { Current.user }
   belongs_to :user, optional: true
+
+  scope :recent_first,   -> { order(date: :desc) }
 end
