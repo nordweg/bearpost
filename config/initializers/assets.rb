9d11801fe6e12547c4083dc6d39c530ef9a79af6
Lucas Kuhn <lucasalexkuhn@gmail.com>
2019-09-03 13:54:47 -0300

    precompiles assets

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -13,3 +13,4 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
 Rails.application.config.assets.precompile += %w( keen/skins/aside/navy.css )
+Rails.application.config.assets.precompile += %w( ^[^_]*.css *.css.erb )
