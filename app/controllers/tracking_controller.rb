e80b7638112cbc23bafb2c92e06c7d4dcdd1af67
Lucas Kuhn <lucasalexkuhn@gmail.com>
2019-09-13 16:28:03 -0300

    redirects to tracking url

--- app/controllers/tracking_controller.rb
+++ app/controllers/tracking_controller.rb
@@ -1,6 +1,10 @@
 class TrackingController < ApplicationController
   def show
     @shipment = Shipment.find_by(shipment_number:params[:shipment_number])
-    render :layout => false
+    if @shipment && @shipment.tracking_number
+      redirect_to(@shipment.tracking_url)
+    else
+      render :layout => false
+    end
   end
 end
