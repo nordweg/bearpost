72167e6ff164e384ec5fb86f3d0f4fb1a2a92e47
igorgaelzer <igor@nordweg.com>
2019-09-16 13:55:33 -0300

    Better shipment show

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -21,4 +21,17 @@ module ApplicationHelper
   def carrier_selector
     [['Todas',nil]] + Rails.configuration.carriers.map{|carrier| [carrier.name, carrier.to_s]}
   end
+
+  def status_label(status)
+    class_list = {
+      'pending' => 'kt-badge--metal',
+      'ready' =>'kt-badge--brand',
+      'shipped' => 'kt-badge--success',
+      'canceled' => 'kt-badge--warning'
+    }
+    right_class = class_list[status] || 'kt-badge--metal'
+    "<span class='kt-badge #{right_class} kt-badge--inline kt-badge--pill'>#{ I18n.t(status) }</span>".html_safe
+  end
+
+
 end
