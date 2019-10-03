c33c03d8a75062e62ceb21b1ffc1e772621d20fb
Lucas Kuhn <lucasalexkuhn@gmail.com>
2019-10-03 15:16:54 -0300

    fixes tests

--- spec/requests/dashboard_spec.rb
+++ spec/requests/dashboard_spec.rb
@@ -5,10 +5,10 @@ RSpec.describe "Home", type: :request do
     user = User.create(email: 'test@test.com', password: "password", password_confirmation: "password")
     sign_in user
   end
-  describe "GET /" do
+  describe "GET /dashboard" do
     it "works! (now write some real specs)" do
-      get root_path
-      expect(response).to have_http_status(:redirect)
+      get dashboard_path(date_range:"#{30.days.ago.strftime("%d/%m/%Y")} - #{Date.today.strftime("%d/%m/%Y")}")
+      expect(response).to have_http_status(:success)
     end
   end
 end
