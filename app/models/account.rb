a15cdaa4af1a30cc99c3c002e6e07c86a721ca1c
Lucas Kuhn <lucasalexkuhn@gmail.com>
2019-12-10 16:18:16 -0300

    removes unused method, transmits in new tab

--- app/models/account.rb
+++ app/models/account.rb
@@ -4,10 +4,6 @@ class Account < ApplicationRecord
   has_many :shipments
   has_many :carrier_settings
 
-  def selected_shipping_methods(carrier)
-    carrier.settings['shipping_methods'].map { |k,v| k }
-  end
-
   def carrier_setting_for(carrier)
     carrier_settings.find_by(carrier_class: carrier.to_s)
   end
