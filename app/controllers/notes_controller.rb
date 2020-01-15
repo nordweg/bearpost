73025abd411444a63eba0094bbbb23a6a9e7cdce
Lucas Kuhn <lucasalexkuhn@gmail.com>
2020-01-15 10:05:00 -0300

    simplifies controller

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,14 +1,10 @@
 class NotesController < ApplicationController
   def create
     @note = Note.new(note_params)
-    respond_to do |format|
-      if @note.save
-        format.html { redirect_to @note.shipment, notice: 'Anotação adicionada' }
-        format.json { render :show, status: :created, location: @note }
-      else
-        format.html { render :new }
-        format.json { render json: @note.errors, status: :unprocessable_entity }
-      end
+    if @note.save
+      redirect_to @note.shipment, notice: 'Observação adicionada'
+    else
+      redirect_to @note.shipment, notice: @note.errors.full_messages
     end
   end
 
