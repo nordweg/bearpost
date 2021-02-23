de1ba26ebab378a4c065a10842c1a2b02655b804
Lucas Kuhn <lucasalexkuhn@gmail.com>
2021-02-23 16:13:00 -0300

    fixes the button to delete an user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -45,7 +45,7 @@ class UsersController < ApplicationController
   def destroy
     @user.destroy
     respond_to do |format|
-      format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
+      format.html { redirect_to users_url, notice: 'Usuário apagado' }
       format.json { head :no_content }
     end
   end
